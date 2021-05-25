c75a0d5c64b7925e14f98387d7de89bc6920f15a
Jhonnanthn Balsas <jhoonasw@hotmail.com.br>
2021-05-25 00:58:58 -0300

    Fixing

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const nomeApp = process.env.npm_package_name;
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'node_modules')));
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.use(express.static(`${__dirname}/dist/${nomeApp}`));
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
+});
+
+app.listen(process.env.PORT || 4200);
